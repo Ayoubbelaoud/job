0482576743fac32daedfdb18402c702a04247647
ayoub <ayoubbelaoud@gmail.com>
2026-09-10 14:51:30 +0000

    proxy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
     return (
-        <div>Signin</div>
+        <div>Dash</div>
     )
 }
 export default Dashboard
---
+++ proxy.ts
@@ -0,0 +1,25 @@
+// proxy.ts (فـ الجذر ديال المشروع)
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "@/lib/auth"; // المسار ديال auth.ts عندك
+
+export default async function proxy(request: NextRequest) {
+  const session = await getSession();
+
+  const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+  if (isDashboardPage && !session?.user) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
+
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up"],
+};
